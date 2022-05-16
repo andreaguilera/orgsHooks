d7e1273cce07eac6b9b51dc04d6979257b8881ef
andreaguilera <andre.aguilera55@hotmail.com>
2022-05-16 14:26:08 -0300

    initial setup

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+// import { Container } from './styles';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
